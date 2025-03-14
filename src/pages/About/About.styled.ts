108bb9c9880b3161cc78bc62b6b25a2fb86b6ba1
marianystm <maria.nystm@gmail.com>
2025-03-14 21:11:53 +0100

    fix/about

--- src/pages/About/About.styled.ts
+++ src/pages/About/About.styled.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const AboutWrapper = styled.div`
   min-height: 100vh;
-  padding: 6rem 2rem 2rem;
+  padding: 2rem 2rem 2rem;
   display: flex;
 `;
 
@@ -17,6 +17,7 @@ export const RightSection = styled.div`
   flex-direction: column;
   justify-content: center;
   padding-left: 2rem;
+  margin-top: -4rem;
 `;
 
 export const LeftSection = styled.div`
@@ -25,33 +26,61 @@ export const LeftSection = styled.div`
   flex-direction: column;
   justify-content: center;
   padding-right: 4rem;
+  margin-top: -4rem;
 `;
 
 export const Greeting = styled.h1`
   font-family: "Anton";
-  font-size: 15rem;
+  font-size: 12rem;
   color: var(--accent-color);
   line-height: 1;
   text-align: left;
+  margin-bottom: 0.5rem;
+  letter-spacing: -2px;
+`;
 
-  div {
-    display: flex;
-    gap: 2rem;
-  }
+export const Subtitle = styled.h2`
+  font-family: var(--font-primary);
+  font-size: 1.2rem;
+  color: var(--text-color);
+  font-weight: 400;
+  line-height: 1.4;
+  margin-top: 0;
+  text-transform: uppercase;
+  padding-left: 4rem;
 
-  span {
-    display: inline-block;
-  }
+  a {
+    color: var(--text-color);
+    text-decoration: none;
+    position: relative;
+    transition: color 0.5s ease;
 
-  div + span {
-    display: block;
-    letter-spacing: -3.1px;
-    margin-top: -0.18em;
+    &:after {
+      content: "";
+      position: absolute;
+      width: 100%;
+      height: 1px;
+      bottom: -2px;
+      left: 0;
+      background-color: var(--accent-color);
+      transform: scaleX(0);
+      transform-origin: bottom right;
+      transition: transform 0.5s ease;
+    }
+
+    &:hover {
+      color: var(--accent-color);
+
+      &:after {
+        transform: scaleX(1);
+        transform-origin: bottom left;
+      }
+    }
   }
 `;
 
 export const BioText = styled.div`
-  font-family: "Poppins";
+  font-family: var(--font-primary);
   font-size: 1.2rem;
   line-height: 1.8;
   color: var(--text-color);
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -5,6 +5,7 @@ import {
   LeftSection,
   Greeting,
   BioText,
+  Subtitle,
 } from "./About.styled";
 
 export const About = () => {
@@ -12,41 +13,20 @@ export const About = () => {
     <AboutWrapper>
       <AboutContent>
         <LeftSection>
-          <BioText>
-            <p>
-              Maria Nyström är en Stockholmsbaserad Fullstack-utvecklare med en
-              passion för att skapa lösningar som gör skillnad.
-            </p>
-            <p>
-              Jag trivs bäst i projekt där jag kan vara{" "}
-              <strong>mer än bara en kodare</strong>. Med min kommunikativa
-              förmåga tar jag gärna möten med kunder, samlar information och
-              omsätter affärsbehov till tekniska lösningar. Jag är inte den som
-              bara stirrar in i skärmen - jag ser helheten och förstår hur
-              tekniken kan lösa verkliga problem.
-            </p>
-            <p>
-              Min styrka ligger i att{" "}
-              <strong>snabbt sätta mig in i nya uppgifter</strong> och leverera
-              resultat. Jag är nyfiken, lösningsorienterad och drivs av att
-              ständigt utvecklas. Med rätt stöd och utmaningar kan jag nå hur
-              långt som helst.
-            </p>
-            <p>
-              När jag inte kodar hittar du mig ofta utforskande nya tekniker
-              eller samarbetande med andra kreativa människor för att bygga
-              något spännande.
-            </p>
-          </BioText>
+          <Greeting>Developer</Greeting>
+          <Subtitle>
+            Currently making great things at{" "}
+            <a
+              href="https://www.wndy.se/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Wndy.se
+            </a>
+          </Subtitle>
         </LeftSection>
         <RightSection>
-          <Greeting>
-            <div>
-              <span>Hi!</span>
-              <span>I'm</span>
-            </div>
-            <span>Maria</span>
-          </Greeting>
+          <BioText>{/* Innehåll för höger sektion */}</BioText>
         </RightSection>
       </AboutContent>
     </AboutWrapper>
